e8d4d5cd29214ce0cc9bc17565603785a99cb94f
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2024-02-16 14:59:04 +0530

    added firebase and dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react"
 import { Routes } from "react-router-dom"
-import Signin from "./Components/Signin"
+import Signin from "./Pages/Student/Signin"
 import { Route } from "react-router-dom"
-import Signup from "./Components/Signup"
-import Account from "./Components/Account"
+import Signup from "./Pages/Student/Signup"
+import Account from "./Pages/Student/Account"
+import { AuthContextProvider } from "./Context/AuthContext"
+import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from './Components/ProtectedRoute'
 function App() {
 
   return (
     <>
-    <div className=" text-center text-3xl font-bold">
-    Authentication Page
-    </div>
+
+    <Toaster />
+    <AuthContextProvider>
     <Routes>
     <Route path="/" element={<Signin />} />
     <Route path="/signup" element={<Signup />} />
-    <Route path="/account" element={<Account />} />
+    <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
 
 
     </Routes>
+    </AuthContextProvider>
     </>
   )
 }
