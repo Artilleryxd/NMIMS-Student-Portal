322f4e4a06281eeb9109606a0ab50b01ef6f1548
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2024-02-25 14:43:13 +0530

    added faculty page and charts

---
+++ src/Components/Faculty/Nav.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { UserAuth } from '../../Context/AuthContext';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../../Components/ui/avatar"
+const Nav = () => {
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+
+  const toggleUserDropdown = () => {
+    setIsUserDropdownOpen(!isUserDropdownOpen);
+  };
+  const { user } = UserAuth();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("You are logged out");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="">
+        <div className="max-w-screen-xl mx-auto px-4">
+          <div className="flex items-center justify-between py-4">
+            <div className="flex items-center space-x-3 rtl:space-x-reverse">
+              <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <span className="text-2xl font-semibold whitespace-nowrap text-rose-700 dark:text-rose-700 border-b-white">Portal</span>
+              </a>
+            </div>
+            <div className="hidden md:flex md:w-auto md:items-center md:space-x-8 rtl:space-x-reverse" id="navbar-user">
+              <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0">
+            
+                <li>
+                  <a href="#" className="text-white py-2 px-3 border-b-2 border-transparent " aria-current="page">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white py-2 px-3 border-b-2 border-transparent ">
+                    Attendance
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white py-2 px-3 border-b-2 border-transparent ">
+                    Courses
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white py-2 px-3 border-b-2 border-transparent ">
+                    E Library
+                  </a>
+                </li>
+                <li>
+                  <a               onClick={handleLogout}
+  className="text-white py-2 px-3 border-b-2 border-transparent ">
+                  Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="md:hidden">
+              <button
+                type="button"
+                className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
+                onClick={toggleUserDropdown}
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className={`z-50 ${isUserDropdownOpen ? 'block' : 'hidden'} my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-rose-700 dark:divide-rose-600`} id="user-dropdown">
+        <div className="px-4 py-3">
+          <span className="block text-sm">Welcome</span>
+          <span className="block text-sm">{user && user.email}</span>
+        </div>
+        <ul className="py-2" aria-labelledby="user-menu-button">
+         
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm">
+              Attendace
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm">
+              E Library
+            </a>
+          </li>
+          <li>
+            <a onClick={handleLogout} className="block px-4 py-2 text-sm">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
--- src/Pages/Faculty/Home.jsx
+++ src/Pages/Faculty/Home.jsx
@@ -1,10 +1,52 @@
 import React from "react";
-import Nav from "../../Components/Nav.jsx";
+import Nav from "../../Components/Faculty/Nav.jsx";
+import AreaChartComponent from "../../Components/Faculty/AreaChartComponent.jsx";
+import CardGrid from "../../Components/Faculty/Card.jsx";
+import { UserAuth } from "../../Context/AuthContext.jsx";
+
+
 
 const Home = () => {
+  const { user, logout } = UserAuth();
+
   return (
     <div>
-     <Nav/>
+      <Nav />
+      
+      <div className="flex-row items-center justify-center mt-10 text-center">
+      <a
+            href="#"
+            class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+            role="alert"
+          >
+            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">
+              Faculty
+            </span>{" "}
+            <span class="text-sm font-medium"> Welcome ! {user && user.email}</span>
+            <svg
+              class="ml-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Faculty Dashboard</h1>
+
+
+       <div className=" w-dvh h-96 mt-10 mb-10">
+        <AreaChartComponent/>
+        </div>   
+
+        <div className="flex items-center justify-center mt-10 mb-10">
+        <CardGrid/>
+        </div>
+        </div>
     </div>
   );
 };
