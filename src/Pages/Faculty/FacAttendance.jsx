7be5022a2c29821b2c7f46d6468df934c1f5d819
aKshad <gawdeakshad@gmail.com>
2024-03-19 22:42:15 +0530

    datepicker

--- src/Pages/Faculty/FacAttendance.jsx
+++ src/Pages/Faculty/FacAttendance.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"; // Import the styles
 import Nav from "../../Components/Faculty/Nav";
 import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
 import {
@@ -12,13 +14,19 @@ import {
 import Footer from "../../Components/Footer";
 
 const Facattendance = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div className>
       <Nav />
       <div className="p-3">
         <Card>
           <CardHeader>
-            <CardTitle className="text-center">Attendance</CardTitle>
+            {/* <CardTitle className="text-center">Attendance</CardTitle> */}
             <CardContent>
               <div className="flex mt-2 justify-between">
                 <div className="border p-2 mx-3 my-3 w-36 rounded text-center">
@@ -46,7 +54,6 @@ const Facattendance = () => {
                       <DropdownMenuItem>WP</DropdownMenuItem>
                       <DropdownMenuItem>MPMC</DropdownMenuItem>
                       <DropdownMenuItem>CN</DropdownMenuItem>
-                      <DropdownMenuItem>COA</DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
@@ -54,12 +61,12 @@ const Facattendance = () => {
                   <DropdownMenu>
                     <DropdownMenuTrigger>Date</DropdownMenuTrigger>
                     <DropdownMenuContent>
-                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>Profile</DropdownMenuItem>
-                      <DropdownMenuItem>Billing</DropdownMenuItem>
-                      <DropdownMenuItem>Team</DropdownMenuItem>
-                      <DropdownMenuItem>Subscription</DropdownMenuItem>
+                      <DatePicker
+                        selected={selectedDate}
+                        onChange={handleDateChange}
+                        dateFormat="MM/dd/yyyy"
+                        className="text-black"
+                      />
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
